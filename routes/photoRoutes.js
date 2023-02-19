509a45c4965ad663c5a66ebfeaf95a13bd4c400f
Nicholas Otieno <niklasoti@gmail.com>
2023-02-19 02:46:56 +0100

    Part III - Advanced Routes: Return Combined Data

--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -47,4 +47,37 @@ router.route("/:id").get(async (req, res) => {
   }
 });
 
+router.route("/user/:username").get(async (req, res) => {
+  const { username } = req.params;
+  try {
+    const response = await axios({
+      method: "get",
+      url: `/users/${username}/photos`,
+      baseURL,
+      headers,
+    });
+
+    const data = response.data.map((obj) => {
+      const {
+        id,
+        user: { username },
+        urls: { raw },
+        description,
+      } = obj;
+      return {
+        id,
+        username,
+        description: description ?? "No description provided.",
+        url: raw,
+      };
+    });
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(error.response.status).json({
+      message: error.message,
+    });
+  }
+});
+
 export default router;
